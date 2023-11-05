01865666cd019d6825219127fcadc39a36630752
dilshod0076 <zamira8888@gmail.com>
2023-11-05 15:10:55 +0500

    Update

--- src/views/CreateProduct.vue
+++ src/views/CreateProduct.vue
@@ -1,12 +1,21 @@
 <template>
 	<div >
-		<h1>
-			CreateProduct
-		</h1>
+		<h1>CreateProduct</h1>
+		<button @click="saveProduct">Сохранить</button>
 	</div>
 </template>
 <script>
 export default{
-	
+	data() {
+    return {
+      product: {
+      },
+    };
+  },
+  methods: {
+    saveProduct() {
+		this.$router.push('/');
+    },
+  },
 }
 </script>
